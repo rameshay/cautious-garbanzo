62bbf71bd5b13810c793d60d2672b10cb051fb27
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-05-23 14:57:15 -0700

    Build and push to docker registry

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -4,9 +4,9 @@ for dir in $dirs; do
     pushd $dir
     echo "$PWD building..."
     ./build_docker_image.sh 
-#    echo "sudo docker tag $dir:1.0 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0"
-#    sudo docker tag $dir:1.0 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0
-#    echo "sudo docker push 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0"
-#    sudo docker push 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0
+    echo "sudo docker tag $dir:latest 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0"
+    sudo docker tag $dir:latest 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0
+    echo "sudo docker push 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0"
+    sudo docker push 730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-$dir:1.0
     popd
 done
